7311a32a7bd54656247d868ff4c348588f84ed4f
Yves Hoebeke <yves.hoebeke@gmail.com>
2015-03-24 15:36:39 -0400

    Update getdistance.py

--- NMEA/navigation/getdistance.py
+++ NMEA/navigation/getdistance.py
@@ -1,17 +1,16 @@
 from math import radians, cos, sin, asin, sqrt
 
-def haversine(lon1, lat1, lon2, lat2):
+def haversine(lon1, lat1, lon2, lat2, unit):
     """
     Calculate the great circle distance between two points 
     on the earth (specified in decimal degrees)
     """
     # convert decimal degrees to radians 
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-
+    r = {'m':3956, 'k':6371, 'n':3444}
     # haversine formula 
     dlon = lon2 - lon1 
     dlat = lat2 - lat1 
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
     c = 2 * asin(sqrt(a)) 
-    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
-    return c * r
+    return (a, c * r[unit])
